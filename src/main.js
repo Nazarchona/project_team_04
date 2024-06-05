dc6db47069804ae206cd2fa6d28f762ecb800a60
PavloHohish <pasha.steam@ukr.net>
2024-06-05 17:35:22 +0300

    fix path and form validation

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import './js/reviews.js';
 import './js/header.js';
 import './js/hero.js';
 import './js/contact-section-js/contact-form.js';
-import './js/scroll-to-top';
+import './js/scroll-to-top.js';
